00558b126ef9dee7c7b7a340bbd51a1ece594787
Lajugie, Julien <jlajugie@illumina.com>
2021-04-02 18:00:47 -0700

    feat: fail if files were added

--- add_missing_init.py
+++ add_missing_init.py
@@ -10,7 +10,7 @@ def add_missing_init_files_from_path(
     folders_to_ignore: List[str],
     source_extensions: List[str],
     folder_trees_to_ignore: List[str],
-) -> None:
+) -> bool:
     """
     Add missing __init__.py files to the specified root directories and subdirectories that contain at least one python
     module (the python module does not have to be in the directory directly, it can be in a subdirectory.
@@ -28,21 +28,23 @@ def add_missing_init_files_from_path(
 
     Returns
     -------
-    None
+    bool
+        True if some init files were added, false otherwise.
     """
-    if isinstance(paths, str):
-        paths = [paths]
+    init_file_added = False
 
     for path in paths:
         if not os.path.exists(path):
             exit(f"Cannot find path {path}")
         root_directory = path if os.path.isdir(path) else os.path.dirname(path)
-        add_missing_init_files(
+        init_file_added = init_file_added or add_missing_init_files(
             root_directory,
             folders_to_ignore,
             source_extensions,
             folder_trees_to_ignore,
         )
+
+    return init_file_added
 
 
 def add_missing_init_files(
@@ -50,7 +52,7 @@ def add_missing_init_files(
     folders_to_ignore: List[str],
     source_extensions: List[str],
     folder_trees_to_ignore: List[str],
-) -> None:
+) -> bool:
     """
     Add missing __init__.py files to the specified root directory and subdirectories that contain at least one python
     module (the python module does not have to be in the directory directly, it can be in a subdirectory.
@@ -68,12 +70,15 @@ def add_missing_init_files(
 
     Returns
     -------
-    None
+    bool
+        True if some init files were added, false otherwise.
     """
     if not os.path.exists(root_directory):
         exit(f"Cannot find directory {root_directory}")
     if not os.path.isdir(root_directory):
         exit(f"{root_directory} is not a directory")
+
+    init_file_added = False
 
     for dirpath, _, filenames in os.walk(root_directory):
         if dirpath in folders_to_ignore:
@@ -85,6 +90,9 @@ def add_missing_init_files(
         ):
             print(f"Adding {INIT_FILE_NAME} in {dirpath}")
             open(os.path.join(dirpath, INIT_FILE_NAME), "a").close()
+            init_file_added = True
+
+    return init_file_added
 
 
 def directory_contains_python(
@@ -106,7 +114,8 @@ def directory_contains_python(
 
     Returns
     -------
-    A bool indicating if the specified folder contain a python file.
+    bool
+        True if the specified folder contain a python file, false otherwise.
     """
     for dirpath, dirnames, filenames in os.walk(root_directory):
         if any(
@@ -160,9 +169,13 @@ def main() -> None:
     folders_to_ignore = args.folders_to_ignore.split(",")
     source_extensions = args.source_extensions.split(",")
     folder_trees_to_ignore = args.folder_trees_to_ignore.split(",")
-    add_missing_init_files_from_path(
+
+    init_added = add_missing_init_files_from_path(
         args.source_tree,
         folders_to_ignore,
         source_extensions,
         folder_trees_to_ignore,
     )
+
+    if init_added:
+        exit(1)
